df451195ccaa298e266405cfd2ed698a9b466c8c
AnishBastakoti <anishbastakoti7@gmail.com>
2020-10-15 21:38:06 +0930

    made changes to map

--- build/js/book.js
+++ build/js/book.js
@@ -24,3 +24,19 @@ var slideIndex = 1;
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
   }
+var map;
+	function initMap() {
+       map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: -34.397, lng: 150.644},
+       zoom: 9
+    });
+		
+var marker = new google.maps.Marker({
+   position: {lat: -12.371809, lng: 130.868912},
+   map: map,
+   title: 'CDU Library',
+   animation: google.maps.Animation.BOUNCE,
+   draggable: true
+ });
+
+}
